92c317855594001f30fe32634b79b845152574f1
Matej <ruzicka.maty@gmail.com>
2025-01-23 23:57:16 +0100

    backported to 1.20.4

---
+++ common/src/main/java/com/mars/sixstairs/Constants.java
@@ -0,0 +1,11 @@
+package com.mars.sixstairs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "sixstairs";
+    public static final String MOD_NAME = "Craft 6 Stairs Instead of 4";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
