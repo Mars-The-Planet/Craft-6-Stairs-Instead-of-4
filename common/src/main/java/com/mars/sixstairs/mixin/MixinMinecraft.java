b3574656c0af01f1dfa5191086a36ffa7e82b0c5
Matej <ruzicka.maty@gmail.com>
2025-03-22 19:45:39 +0100

    fixed deps

--- common/src/main/java/com/mars/sixstairs/mixin/MixinMinecraft.java
+++ common/src/main/java/com/mars/sixstairs/mixin/MixinMinecraft.java
@@ -1,16 +1,32 @@
 package com.mars.sixstairs.mixin;
 
-import com.mars.sixstairs.Constants;
-import net.minecraft.client.Minecraft;
+import com.google.gson.JsonElement;
+import com.mars.sixstairs.SixStairsConfig;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraft.world.item.crafting.RecipeManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
-public class MixinMinecraft {
+import java.util.Map;
+import java.util.Objects;
 
-    @Inject(at = @At("TAIL"), method = "<init>")
-    private void init(CallbackInfo info) {
+@Mixin(RecipeManager.class)
+public class MixinMinecraft {
+    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("HEAD"))
+    public void apply(Map<ResourceLocation, JsonElement> object, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci){
+        for (int i = 0; i < SixStairsConfig.blockStairsList.size(); i++) {
+            String current = SixStairsConfig.blockStairsList.get(i);
+            String[] set = ((current).replaceAll("\\s","")).split(",");
+            for(ResourceLocation resourceLocation : object.keySet()){
+                if(Objects.equals(resourceLocation, ResourceLocation.parse(set[1]))){
+                    object.remove(resourceLocation);
+                    break;
+                }
+            }
+        }
     }
 }
